12f3c2511d10ee626dcc3185e64d2342b2fe80b3
Adam Nowland <acnowland@gmail.com>
2020-12-18 18:46:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,14 +88,10 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto] = {}
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Unix"
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-    puts programmer_hash
+    programmer_hash
 
 end
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -116,6 +112,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
