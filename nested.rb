c4ed29ce230fd770e8cdb7a0f6c39810222fdd82
Adam Nowland <acnowland@gmail.com>
2020-12-18 18:40:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,11 @@ def adding_matz
       }
     }
     
-    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby"
+      :languages => ["LISP", "C"]
+    }
+    programmer_hash
 
 end
 # :yukihiro_matsumoto => {
